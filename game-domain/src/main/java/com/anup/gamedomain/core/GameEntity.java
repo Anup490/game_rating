b669c64f98ffd24fd73ff83c14e91d1e4c85f668
Anup490 <anupbhandari490@gmail.com>
2020-06-27 14:27:49 +0545

    Successfully completed integration with hibernate and add sequence generator to generate primary key for GameEntity

--- game-domain/src/main/java/com/anup/gamedomain/core/GameEntity.java
+++ game-domain/src/main/java/com/anup/gamedomain/core/GameEntity.java
@@ -1,14 +1,15 @@
 package com.anup.gamedomain.core;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "games")
 public class GameEntity {
 
-    @Id private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @SequenceGenerator(name = "sequence_generator", sequenceName = "hibernate_sequence", allocationSize = 1)
+    private int id;
     private String name;
     private byte[] photo;
     private String description;
